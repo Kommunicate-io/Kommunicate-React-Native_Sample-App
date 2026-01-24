75caf4874eb994b4960ae8433535831bbe16d800
Adarsh Kumar <adarsh@kommunicate.io>
2026-01-24 22:50:22 +0530

    Updating sample app to support new versions

--- android/app/src/main/java/com/kommunicatereactnativesampleapp/MainApplication.java
+++ android/app/src/main/java/com/kommunicatereactnativesampleapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.content.res.Configuration;
 import androidx.annotation.NonNull;
+import androidx.multidex.MultiDex;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -59,6 +60,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    MultiDex.install(this);
     // If you opted-in for the New Architecture, we enable the TurboModule system
     ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
